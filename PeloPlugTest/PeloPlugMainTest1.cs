80a4f6ebeaff32f9b4f152dc1898bcdeb1efde18
kenceglia <kenceglia@hotmail.com>
2022-06-09 19:11:30 -0700

    add tests

---
+++ PeloPlugTest/PeloPlugMainTest1.cs
@@ -0,0 +1,23 @@
+using PeloPlug;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PeloPlugTest;
+
+[TestClass]
+public class UnitTestPeloPlug
+{
+    [TestMethod]
+    public async Task Authenticate_Success()
+    {
+        IPelo iPelo = new PeloClient();
+        var b = await iPelo.GetUserIDSession("KenCeglia@hotmail.com", "Denver.12k");
+        Assert.IsTrue(b);
+    }
+    [TestMethod]
+    public async Task Authenticate_Fail()
+    {
+        IPelo iPelo = new PeloClient();
+        var b = await iPelo.GetUserIDSession("XKenCeglia@hotmail.com", "Denver.12k");
+        Assert.IsFalse(b);
+    }
+}
